7d09af417efad6aa3f04c8db012a26e0904f7d18
lukeselinger <lukeselinger@FTS-users-iMac.local>
2015-10-15 17:53:51 -0400

    null card type js

--- build/assets/js/script.js
+++ build/assets/js/script.js
@@ -1,9 +1,12 @@
     
 $('#Card_Number').validateCreditCard(function(result){
 
-				$("#" + result.card_type.name).prop("checked", true);
+				if (result.card_type === null){ $('.cc input').prop("checked",false)
+			}
+				else{ 
+					$("#" + result.card_type.name).prop("checked", true);
+			showAmex();}
 
-			showAmex();
 		});
     
 
